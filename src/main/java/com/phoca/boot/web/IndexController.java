dd91ecd41eb2baa59a981dde15ecbe229206b1d2
HyunJay52 <hyunjungk52@gmail.com>
2022-01-20 00:01:59 +0900

    add path on annotation

--- src/main/java/com/phoca/boot/web/IndexController.java
+++ src/main/java/com/phoca/boot/web/IndexController.java
@@ -12,7 +12,7 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping
+    @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
     }
