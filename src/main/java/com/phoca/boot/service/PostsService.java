bac770fd6f626f01fcd9042259a463cf2628376a
HyunJay52 <hyunjungk52@gmail.com>
2022-01-21 02:09:25 +0900

    done for update

--- src/main/java/com/phoca/boot/service/PostsService.java
+++ src/main/java/com/phoca/boot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.phoca.boot.service;
 
 import com.phoca.boot.domain.posts.Posts;
 import com.phoca.boot.domain.posts.PostsRepository;
+import com.phoca.boot.web.dto.PostsListResponseDto;
 import com.phoca.boot.web.dto.PostsResponseDto;
 import com.phoca.boot.web.dto.PostsSaveRequestDto;
 import com.phoca.boot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,9 +33,18 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly=true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다 id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly=true) //readOnly: 트랜잭션 범위 유지, 조회기능만 남김 -> 조회속도 개선, 등록/수정/삭제 기능이 없을 경우 사용추천
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+        //.map(posts->new PostsListResponseDto(posts)) Dto로 변환하고 List로 반환하는 메소드
     }
 }
