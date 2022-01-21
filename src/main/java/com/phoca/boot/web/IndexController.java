909dbbecf7e21fa3a2605ca5b7af7f1ea432e4d1
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-01-22 03:20:52 +0900

    add join

--- src/main/java/com/phoca/boot/web/IndexController.java
+++ src/main/java/com/phoca/boot/web/IndexController.java
@@ -39,4 +39,7 @@ public class IndexController {
 
     @GetMapping("/market/market_list")
     public String market() { return "/admin/board/market_list"; }
+
+    @GetMapping("/member/join")
+    public String join() { return "/guest/member/join"; }
 }
