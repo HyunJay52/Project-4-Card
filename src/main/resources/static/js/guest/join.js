3fc3ab690d2e1da696d621e7f2b112f728f6b266
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-01-22 20:27:18 +0900

    edit join

--- src/main/resources/static/js/guest/join.js
+++ src/main/resources/static/js/guest/join.js
@@ -1,10 +1,9 @@
 function validform() {
 
-        var a = document.forms["my-form"]["full-name"].value;
-        var b = document.forms["my-form"]["email-address"].value;
-        var c = document.forms["my-form"]["username"].value;
-        var d = document.forms["my-form"]["permanent-address"].value;
-        var e = document.forms["my-form"]["nid-number"].value;
+        var a = document.forms["my-form"]["email-address"].value;
+        var b = document.forms["my-form"]["username"].value;
+        var c = document.forms["my-form"]["password"].value;
+        var d = document.forms["my-form"]["phone_number"].value;
 
         if (a==null || a=="")
         {
@@ -21,10 +20,6 @@ function validform() {
         }else if (d==null || d=="")
         {
             alert("Please Enter Your Permanent Address");
-            return false;
-        }else if (e==null || e=="")
-        {
-            alert("Please Enter Your NID Number");
             return false;
         }
 
