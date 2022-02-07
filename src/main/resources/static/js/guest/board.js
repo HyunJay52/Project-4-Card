141fb6e5ce7d256c596e6aa3d09e2c6457501b36
HyunJay52 <hyunjungk52@gmail.com>
2022-02-07 17:55:22 +0900

    working on pagination

--- src/main/resources/static/js/guest/board.js
+++ src/main/resources/static/js/guest/board.js
@@ -51,8 +51,8 @@ var board_main = {
 
     // pagination
     $('.pagination_div').twbsPagination({
-    		totalPages: 35,	// 총 페이지 번호 수
-		    visiblePages: 7,	// 하단에서 한번에 보여지는 페이지 번호 수
+    		totalPages: 10,	// 총 페이지 번호 수
+		    visiblePages: 5,	// 하단에서 한번에 보여지는 페이지 번호 수
 		    startPage : 1, // 시작시 표시되는 현재 페이지
 		    initiateStartPageClick: false,	// 플러그인이 시작시 페이지 버튼 클릭 여부 (default : true)
 		    first : "<<",	// 페이지네이션 버튼중 처음으로 돌아가는 버튼에 쓰여 있는 텍스트
